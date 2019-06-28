b4a0062d44d38a440de68da26ba9fdec5512d77a
Genaro Tinoco <genaro_13@hotmail.es>
2019-06-27 21:35:34 -0600

    Adding Error override global interface

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -380,8 +380,8 @@ export class UserController {
             .then((result: any) => {
                 res.status(200)
                     .json(result);
-            }).catch((error: CNodeJs.Error) => {
-            res.status(error.status | 500)
+            }).catch((error: Error) => {
+            res.status(error.status || 500)
                 .json(error)
         })
     };
