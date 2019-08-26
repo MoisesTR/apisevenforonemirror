822075623c102086d435e671a7352cb6e07e6c22
mtrigueros <moisestrigueros@hotmail.com>
2019-08-25 23:25:27 -0600

    Add features update and get image

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -35,7 +35,7 @@ export const register = (server: server) => {
         .post('/recover', validations.recoverAccount, authController.forgotAccount)
         .get('/email/:userName', validations.getEmail, authController.getEmailByUserName)
         .post('/admin', ensureAuth, validations.createAdmin, validsParams, authController.createAdminUser)
-        .get('/getImage/:folder/:img', authController.getImage);
-        // .post('/upload', ensureAuth, authController.upload);
+        .get('/getImage/:folder/:img', ensureAuth, authController.getImage)
+        .put('/upload/:folder/:id', ensureAuth, validations.uploadImage, validsParams , authController.upload);
     app.use('/api/auth', router);
 };
