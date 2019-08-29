4c94bbd8f675216cc0634259757b01adca6760df
Genaro Tinoco <genaro_13@hotmail.es>
2019-08-28 22:01:36 -0600

    Emitir evento de NOTIFICATION si el usuario al que pertenece se encuentra conectado

--- src/db/interfaces/INotification.ts
+++ src/db/interfaces/INotification.ts
@@ -1,15 +1,18 @@
 import {ObjectId} from 'bson';
 import {Document, Model} from 'mongoose';
 import {IRole} from './IRole';
+import {ENotificationTypes} from '../models/Notification';
 
 export interface INotification {
-    idUser: ObjectId;
-    idGroup?: ObjectId;
+    userId: ObjectId;
+    notificationType: ENotificationTypes;
     content: string;
+    read: boolean;
+    groupId:  ObjectId | string;
     createdAt: Date;
     updatedAt: Date;
 }
 
-export interface INotificationDocument extends IRole, Document {}
+export interface INotificationDocument extends INotification, Document {}
 
 export interface INotificationModel extends Model<INotificationDocument> {}
