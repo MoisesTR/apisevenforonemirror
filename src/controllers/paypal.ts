2b35e599860cb286f864e549f3fc075bea3c05bc
mtrigueros <moisestrigueros@hotmail.com>
2019-10-22 22:25:55 -0600

    Add routes paypal

--- src/controllers/paypal.ts
+++ src/controllers/paypal.ts
@@ -18,13 +18,14 @@ import catchAsync from '../utils/catchAsync';
 // NAME ERROR PAYPAL
 const INSUFFICIENT_FUNDS = 'INSUFFICIENT_FUNDS';
 import logger from '../services/logger';
-// 1. Set up your server to make calls to PayPal
 
+// 1. Set up your server to make calls to PayPal
 // 2. Set up your server to receive a call from the client
 export const createPaypalTransaction = catchAsync(
     async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
         // 3. Call PayPal to set up a transaction
         const finalPrice = req.body.finalPrice;
+        const groupId = req.body.groupId;
 
         const moneyCode = 'USD';
         const descriptionItem = 'Inversion en grupo de juego 7x1';
@@ -48,6 +49,11 @@ export const createPaypalTransaction = catchAsync(
             const request = createRequest(moneyCode, finalPrice.toString(), nameItemBuy, descriptionItem);
 
             order = await client().execute(request);
+
+            await getToken(next).then((body: any) => {
+                const bodyParsed = JSON.parse(body);
+                console.log('token', bodyParsed.access_token);
+            });
         } catch (err) {
             // 4. Handle any errors from the call
             logger.error('Error Paypal', err);
@@ -109,57 +115,61 @@ function createRequest(moneyCode: string, finalPrice: number, nameItemBuy: strin
     return request;
 }
 
-export const createAuthorizationTransaction = catchAsync(
-    async (req: Express.Request, res: Express.Response) => {
-        // 2a. Get the order ID from the request body
-        const orderID = req.body.orderID;
+export const captureTransaction = catchAsync(async (req: Express.Request, res: Express.Response) => {
+    // 2a. Get the order ID from the request body
+    const orderID = req.body.orderID;
 
-        // 3. Call PayPal to create the authorization
-        const request = new checkoutNodeJssdk.orders.OrdersAuthorizeRequest(orderID);
-        let authorizationID;
-        request.requestBody({});
-
-        try {
-            const authorization = await client().execute(request);
-
-            // 4. Save the authorization ID to your database
-            authorizationID = authorization.result.purchase_units[0].payments.authorizations[0].id;
-            // await database.saveAuthorizationID(authorizationID);
-        } catch (err) {
-            // 5. Handle any errors from the call
-            console.error(err);
-            return res.send(500);
-        }
-
-        // 6. Return a successful response to the client
-        return res.send(200);
-        // return res.send(200).json({
-        //     authorizationID: authorizationID
-        // });
-    },
-);
-
-export const captureAuthorization = catchAsync(async (req: Express.Request, res: Express.Response) => {
-    // 2. Get the authorization ID from your database
-    const authorizationID = req.body.authorizationID;
-
-    // 3. Call PayPal to capture the authorization
-    const request = new checkoutNodeJssdk.payments.AuthorizationsCaptureRequest(authorizationID);
+    // 3. Call PayPal to capture the order
+    const request = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderID);
     request.requestBody({});
+
     try {
         const capture = await client().execute(request);
 
-        // 4. Save the capture ID to your database for future reference.
+        // 4. Save the capture ID to your database. Implement logic to save capture to your database for future reference.
         const captureID = capture.result.purchase_units[0].payments.captures[0].id;
+        console.log({captureID});
         // await database.saveCaptureID(captureID);
     } catch (err) {
         // 5. Handle any errors from the call
         console.error(err);
-        return res.send(500);
+        return res.status(500);
     }
 
     // 6. Return a successful response to the client
-    return res.send(200);
+    res.status(200).json({
+        message: 'Capture Transaction sucessfully!',
+    });
+});
+
+export const getOrderDetails = catchAsync(async (req: Express.Request, res: Express.Response) => {
+    // 2a. Get the order ID from the request body
+    const orderID = req.params.orderID;
+
+    console.log({orderId: orderID});
+
+    // 3. Call PayPal to get the transaction details
+    const request = new checkoutNodeJssdk.orders.OrdersGetRequest(orderID);
+
+    let order;
+    try {
+        order = await client().execute(request);
+    } catch (err) {
+        // 4. Handle any errors from the call
+        console.error(err);
+        return res.status(500);
+    }
+
+    // // 5. Validate the transaction details are as expected
+    // if (order.result.purchase_units[0].amount.value !== '220.00') {
+    //     res.status(400);
+    // }
+
+    // 6. Save the transaction in your database
+    // await database.saveTransaction(orderID);
+
+    // 7. Return a successful response to the client
+    res.status(200).json({order});
 });
 
 export const payout = catchAsync(
